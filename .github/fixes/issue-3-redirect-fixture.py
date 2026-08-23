eba9125e7c532ba0a453f3ed193a0a63212477b7
TimWhite <36320909+timwhitez@users.noreply.github.com>
2026-08-23 21:04:17 +0900

    test: exercise private redirect through CheckRedirect callback

--- .github/fixes/issue-3-redirect-fixture.py
+++ .github/fixes/issue-3-redirect-fixture.py
@@ -23,20 +23,22 @@ old = '''\torigTransport := http.DefaultTransport
 '''
 new = '''\torigDo := webfetchDoRequest
 \tvar calls int
-\twebfetchDoRequest = func(_ *http.Client, r *http.Request) (*http.Response, error) {
+\twebfetchDoRequest = func(client *http.Client, r *http.Request) (*http.Response, error) {
 \t\tcalls++
 \t\tif calls > 1 {
 \t\t\tt.Fatalf("redirect request should not run for blocked private destination")
 \t\t}
-\t\tresp := &http.Response{
-\t\t\tStatus:     "302 Found",
-\t\t\tStatusCode: http.StatusFound,
-\t\t\tHeader:     make(http.Header),
-\t\t\tBody:       io.NopCloser(strings.NewReader("redirect")),
-\t\t\tRequest:    r,
+\t\tredirectRequest, requestErr := http.NewRequestWithContext(r.Context(), http.MethodGet, "http://internal.test/private", nil)
+\t\tif requestErr != nil {
+\t\t\treturn nil, requestErr
 \t\t}
-\t\tresp.Header.Set("Location", "http://internal.test/private")
-\t\treturn resp, nil
+\t\tif client.CheckRedirect == nil {
+\t\t\treturn nil, errors.New("WebFetch client has no redirect policy")
+\t\t}
+\t\tif redirectErr := client.CheckRedirect(redirectRequest, []*http.Request{r}); redirectErr != nil {
+\t\t\treturn nil, redirectErr
+\t\t}
+\t\treturn nil, errors.New("private redirect was unexpectedly accepted")
 \t}
 \tt.Cleanup(func() { webfetchDoRequest = origDo })
 '''
